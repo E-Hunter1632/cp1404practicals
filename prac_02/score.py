0c9b43972d1d965dd12709c7b77f5dc020ee33b8
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-09-21 13:30:09 +1000

    Add score calc from random number

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,11 +3,17 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+import random
+
 
 def main():
     score = float(input("Enter score: "))
     print(score)
     print(determine_result(score))
+
+    random_num = random.randint(0, 100)
+    print(random_num)
+    print(determine_result(random_num))
 
 
 def determine_result(score):
