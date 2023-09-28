acad72263ebe0bc62b064e469bbe026d42715ac8
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-09-28 11:44:52 +1000

    change min, max and max increase values

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -11,10 +11,10 @@ import random
 
 number_of_days = 1
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
