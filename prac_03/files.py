48b62770c5afceafd4b6c9b5eee9fcc185af3b44
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-09-28 12:14:44 +1000

    Complete prac task 2.

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,3 +5,8 @@ out_file = open(filename, 'w')
 print(name, file=out_file)
 out_file.close()
 
+# TASK 2:
+in_file = open(filename, 'r')
+for line in in_file:
+    print(f"Your name is {line}")
+in_file.close()
