480276bdea09d42b8a8de72f555bd79a33b28482
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-09-28 13:00:32 +1000

    Remove todo markers as completed.

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -7,10 +7,9 @@ is_finished = False
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        # TODO: this line - DONE
         is_finished = True
     except ValueError:
-        # TODO - add the exception you want to catch after except - DONE
+        # add the exception you want to catch after except - DONE
         print("Please enter a valid integer.")
 print("Valid result is:", result) # This is safe to ignore.
 is_finished = True
