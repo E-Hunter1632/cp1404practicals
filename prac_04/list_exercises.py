8ea1aaa4f28def708473dfb829145a3687fc5bca
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-05 12:03:00 +1000

    Correct 1st task average calculation.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,7 +9,8 @@ print("The first number is: ", numbers[0])
 print("The last number is: ", numbers[-1])
 print("The smallest number is: ", min(numbers))
 print("The largest number is: ", max(numbers))
-print("The average of numbers is: ", sum(numbers) / 2)
+print("The average of numbers is: ", sum(numbers) / len(numbers))
+
 
 # 2nd Exercise:
 
