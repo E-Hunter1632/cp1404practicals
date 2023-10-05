6559a41b24b69109054240b38c66915ac3347ce3
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-05 12:05:43 +1000

    Complete username checker.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,5 +12,11 @@ print("The largest number is: ", max(numbers))
 print("The average of numbers is: ", sum(numbers) / len(numbers))
 
 
-# 2nd Exercise:
-
+# 2nd Exercise: Inadequate security checker.
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter username: ")
+if username in usernames:
+    print("Access Granted.")
+else:
+    print("Access Denied.")
