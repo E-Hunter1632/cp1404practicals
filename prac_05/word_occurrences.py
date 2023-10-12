cf663293dd1b2c75c213e04ae936957d195dd3d9
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-12 12:07:35 +1000

    Fix and complete functioning code

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,12 +5,15 @@ Actual Completion Time:
 """
 string = str(input("Enter a sentence to evaluate word occurrences: "))
 word_to_count = {}
-words = string.split(" ")
-for word in words:
+split_words = string.split(" ")
+for word in split_words:
     try:
         word_to_count[word] += 1
     except KeyError:
         word_to_count[word] = 1
 
-for word in words:
+split_words = list(word_to_count.keys())
+split_words.sort()
+
+for word in split_words:
     print(f"{word} : {word_to_count[word]}")
