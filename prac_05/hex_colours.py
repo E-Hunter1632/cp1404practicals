1ca61ade853f2fff0ab1130efaac65473aa74eca
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-12 11:40:44 +1000

    Complete functioning program.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,21 @@
+"""Hex colour dictionary"""
+
+COLOUR_TO_HEX = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": "#f0f8ff",
+                 "alizarincrimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00",
+                 "amethyst": "#9966cc", "antiquewhite": "#faebd7", "apricot": "#fbceb1",
+                 "aqua": "#00ffff"}
+
+colour = str(input("Enter a colour: ")).lower()
+while colour != "":
+    # try:
+    #     colour = str(input("Enter a colour: ")).lower()
+    # except ValueError:
+    #     print("Invalid input. Enter a valid")
+    #     colour = str(input("Enter a colour: ")).lower
+    try:
+        print(COLOUR_TO_HEX[colour])
+        colour = str(input("Enter a colour to receive the hex equivalent: ")).lower
+    except KeyError:
+        print("Colour not in dictionary. Enter a different colour.")
+        colour = str(input("Enter a colour to receive the hex equivalent: ")).lower()
+
