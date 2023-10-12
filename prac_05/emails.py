998546a1680fad5c77857394d4132d27f1b204f4
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-12 13:27:38 +1000

    Fix split function for email.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,15 +1,15 @@
 """
 Enter emails and names
 Estimated completion time: 20-25 minutes.
-Actual completion time: (Started at 15 past - Paused at )
+Actual completion time: (Started at 15 past - Paused at 58 past)
 """
 email_to_name = {}
 email = str(input("Enter your email: "))
 while email != "":
     split_1 = email.split("@")
-    split_2 = email.split(".")
+    split_2 = split_1[0].split(".")
     name = " ".join(split_2).title()
-    selection = str(input(f"Is your name {name}? (Y/n)"))
+    selection = str(input(f"Is your name {name}? (Y/n) "))
     if selection.upper() != 'Y' and selection != "":
         name = str(input("Enter your name: "))
     email_to_name[email] = name
