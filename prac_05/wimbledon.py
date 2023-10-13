b69587f7076ceff95886ff6ac12adf53dc206532
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-13 11:18:41 +1000

    Add file reading for getting data ready for processing.

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,25 @@
+"""
+Wimbledon game matches
+Estimated Completion Time: 30-40 minutes
+Actual Completion Time:
+"""
+
+
+def main():
+    data = get_data()
+    print(data)
+
+
+def get_data():
+    """Read the file and prepare contents for processing"""
+    data = []
+    in_file = open("wimbledon.csv", 'r')
+    in_file.readline()  # reads and skips the first line.
+    for line in in_file:
+        parts = line.strip().split(',')  # removes the \n and puts into parts
+        data.append(parts)  # adds parts to list for later processing
+    in_file.close()
+    return data
+
+
+main()
