6d8ab9461887b0027ed91ce5a11c6aac8b348f30
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-26 12:49:04 +1000

    Add class and main program.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,6 +1,22 @@
 """
 Creating new programming language class.
 Estimated completion time: 30-40 minutes (start 11:50)
-Actual completion time:
+Actual completion time: (Paused at 12:
 """
+from prac_06.programming_language import ProgrammingLanguage
 
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
