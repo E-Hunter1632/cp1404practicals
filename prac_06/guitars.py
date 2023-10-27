34d1f96cf60b75f2af5f75b5ecf128272435d4e8
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-27 18:25:00 +1000

    Initial commit, start of program

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,5 +1,14 @@
 """
 Program that uses the guitar class object
-Estimated completion time: 40-50 minutes
+Estimated completion time: 40-50 minutes (start 6:10)
 Actual completion time:
 """
+from prac_06.guitar import Guitar
+
+guitars = []
+
+guitar_name = input("Guitar Name: ")
+year = input()
+cost = input()
+guitar = Guitar(guitar_name, year, cost)
+guitars.append(guitar)
