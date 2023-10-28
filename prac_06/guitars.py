6851a36d74960d4418b4d445f869020291e19833
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-28 10:32:29 +1000

    Add printing guitars using guitar class.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,6 @@
 """
 Program that uses the guitar class object
-Estimated completion time: 40-50 minutes (start 6:10)
+Estimated completion time: 40-50 minutes
 Actual completion time:
 """
 from prac_06.guitar import Guitar
@@ -8,7 +8,20 @@ from prac_06.guitar import Guitar
 guitars = []
 
 guitar_name = input("Guitar Name: ")
-year = input()
-cost = input()
-guitar = Guitar(guitar_name, year, cost)
-guitars.append(guitar)
+while guitar_name != '':
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
+    guitar = Guitar(guitar_name, year, cost)
+    guitars.append(guitar)
+    guitar_name = input("Guitar Name: ")
+
+guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = ""
+    if guitar.is_vintage:
+        vintage_string = " (vintage)"
+    else:
+        vintage_string = ""
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
