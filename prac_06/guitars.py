8afe2cccba848c9b611def73856ca22beb580fa8
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-10-28 10:40:00 +1000

    Print as per sample output given.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,16 +7,20 @@ from prac_06.guitar import Guitar
 
 guitars = []
 
+print("My guitars! ")
 guitar_name = input("Guitar Name: ")
 while guitar_name != '':
     year = int(input("Year: "))
     cost = float(input("Cost: "))
     guitar = Guitar(guitar_name, year, cost)
     guitars.append(guitar)
+    print(f"{guitar_name} ({year}) : ${cost:,.2f} added. ")
     guitar_name = input("Guitar Name: ")
+
 
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
 
 for i, guitar in enumerate(guitars, 1):
     vintage_string = ""
