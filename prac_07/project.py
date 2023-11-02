d604db49dd572f5ea5f7eaff0b8154e7d78843f7
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-11-02 12:17:05 +1000

    Add str method to project class.

--- prac_07/project.py
+++ prac_07/project.py
@@ -12,5 +12,9 @@ class Project:
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
+    def __str__(self):
+        """Return formatted string of project details. """
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}")
+
 # Be able to sort/compare projects based on date/priority order - using '<'
-#
