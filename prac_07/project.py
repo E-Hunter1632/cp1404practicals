d8e2671b064260b1e912724e94a20e1610767765
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-11-02 12:12:09 +1000

    Start on Project class initialisation.

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,4 +1,16 @@
 """Project class"""
 
+
+class Project:
+    """Represent a Project class object"""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise the Project object"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
 # Be able to sort/compare projects based on date/priority order - using '<'
 #
