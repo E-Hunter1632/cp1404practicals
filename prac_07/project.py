437a29aa9359a6e7ae7145ba4a875454c7fc6cd4
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-11-04 16:42:39 +1000

    add priority and date sorting into class

--- prac_07/project.py
+++ prac_07/project.py
@@ -12,15 +12,15 @@ class Project:
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
-    def __str__(self):
-        """Return formatted string of project details. """
-        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
-                f"estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}")
-
-    # def __repr__(self):
-    #     """Return formatted string/list of project details"""
+    # def __str__(self):
+    #     """Return formatted string of project details. """
     #     return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
     #             f"estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}")
+
+    def __repr__(self):
+        """Return formatted string/list of project details"""
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate}, completion: {self.completion_percentage}")
 
     def __gt__(self, other):
         """Compare priority - Sort the highest priority -> the lowest priority"""
