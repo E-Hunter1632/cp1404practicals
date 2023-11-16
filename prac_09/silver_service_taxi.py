aa040d42135e8396bdd28042c2269e9a2dfd6704
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-11-16 12:20:35 +1000

    Add flagfall charge. Update string method, Update get_fare method.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -4,7 +4,18 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """SilverServiceTaxi class"""
+    flagfall = 4.50
 
-    def __init__(self, name, fuel):
+    def __init__(self, name, fuel, fanciness=0.0):
         """Initialise the SilverServiceTaxi class object"""
         super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * self.fanciness
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip plus the additional flagfall charge."""
+        return self.flagfall + self.price_per_km * self.current_fare_distance
