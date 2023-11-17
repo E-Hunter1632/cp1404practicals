7fcc4c1c689e7e6931b530f3962151738dba04bc
Eric.W <140286262+E-Hunter1632@users.noreply.github.com>
2023-11-17 15:17:47 +1000

    Add taxi drive ability. Fix correct menu printing.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,6 +8,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     current_taxi = None
+    current_bill = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     print(MENU)
@@ -16,9 +17,15 @@ def main():
         if selection.lower() == 'd':
             if current_taxi is None:
                 print("You need to choose a taxi before you can drive")
-                print("f Bill to date: {Current_Bill}")  # This is just a placeholder for me for later.
+                print(f"Bill to date: ${current_bill:.2f}")  # This is just a placeholder for me for later.
             else:
-                drive()
+                distance = float(input("Drive how far? "))
+                current_taxi.start_fare()
+                current_taxi.drive(distance)
+                current_fare = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${current_fare:.2f}")
+                current_bill += current_fare
+                print(f"Bill to date: ${current_bill:.2f}")
 
         elif selection.lower() == 'c':
             view_taxis(taxis)
@@ -26,28 +33,28 @@ def main():
                 choice = int(input("Choose taxi: "))
             except ValueError:
                 print("Invalid choice")
-                print("f Bill to date: {Current_Bill}")
+                print(f"Bill to date: ${current_bill:.2f}")
             try:
                 current_taxi = taxis[choice]
             except IndexError:
                 print("Invalid taxi choice")
-                print("f Bill to date: {Current_Bill}")
+
+            print(f"Bill to date: ${current_bill:.2f}")
 
         else:
             print("Invalid option")
+            print(f"Bill to date: ${current_bill:.2f}")
         print(MENU)
         selection = input(">>> ")
-    print("ADD -> QUITTED")
+    print(f"Total trip cost: ${current_bill:.2f}")
+    print("Taxis are now:")
+    view_taxis(taxis)
 
 
 def view_taxis(taxis):
     print("Taxis available:")
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
-
-
-def drive():
-    print("DRIVE THE TAXI HERE - CALL THE TAXI DRIVE METHOD")
 
 
 main()
